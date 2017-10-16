2b777029685823ff5e8d5d856c3421f2b161e209
Sander Moolin <sandermoolin@gmail.com>
2017-10-16 09:44:29 -0400

    Example implementation working

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+app.get('/video', (req, res) => {
+    const path = 'assets/sample.mp4'
+    const fileSize = fs.statSync(path).size
+    const range = req.headers.range
+
+    if( range ){
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1
+        const chunkSize = (end-start) + 1
+        const file = fs.createReadStream( path, { start, end } )
+
+        const head = {
+            'Content-Range': `bytes ${ start }-${ end }/${ fileSize }`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunkSize,
+            'Content-Type': 'video/mp4'
+        }
+        res.writeHead(206, head)
+        file.pipe(res)
+
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
+    }
+})
+
+// app.get('/video', function(req, res) {
+//     const path = 'assets/sample.mp4'
+//     const stat = fs.statSync(path)
+//     const fileSize = stat.size
+//     const range = req.headers.range
+//
+//     if (range) {
+//         const parts = range.replace(/bytes=/, "").split("-")
+//         const start = parseInt(parts[0], 10)
+//         const end = parts[1]
+//             ? parseInt(parts[1], 10)
+//             : fileSize-1
+//         const chunksize = (end-start)+1
+//         const file = fs.createReadStream(path, {start, end})
+//         const head = {
+//             'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+//             'Accept-Ranges': 'bytes',
+//             'Content-Length': chunksize,
+//             'Content-Type': 'video/mp4',
+//         }
+//         res.writeHead(206, head);
+//         file.pipe(res);
+//     } else {
+//         const head = {
+//             'Content-Length': fileSize,
+//             'Content-Type': 'video/mp4',
+//         }
+//         res.writeHead(200, head)
+//         fs.createReadStream(path).pipe(res)
+//     }
+// })
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
